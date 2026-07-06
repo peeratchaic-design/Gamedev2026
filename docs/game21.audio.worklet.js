e0dc52b63df10eb3dca97ce77a1d66d064c71725
peeratchaic-design <peeratchai.c@kkumail.com>
2026-07-06 11:21:57 +0700

    Update game21.audio.worklet.js

--- docs/game21.audio.worklet.js
+++ docs/game21.audio.worklet.js
@@ -1,8 +1,10 @@
+/**************************************************************************/
 /*  audio.worklet.js                                                      */
 /**************************************************************************/
 /*                         This file is part of:                          */
 /*                             GODOT ENGINE                               */
 /*                        https://godotengine.org                         */
+/**************************************************************************/
 /* Copyright (c) 2014-present Godot Engine contributors (see AUTHORS.md). */
 /* Copyright (c) 2007-2014 Juan Linietsky, Ariel Manzur.                  */
 /*                                                                        */
